dc0f6a2477a26302d76743da35fd70de6a9b451a
Trystar360 <tristanfrederick360@gmail.com>
2018-12-11 12:19:00 -0500

    doesnt really work

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ if(w < h){
 $(".gameBoard").width(gameDim - 80);
 $(".gameBoard").height(gameDim - 80);
 console.log(gameDim);
+var alph = ["A","B","C","D","E","F","G","H"];
+function makeTable(x){
+  for(var i = 0; i < x; i++){
+  $("#gameTable").append('<tr class="cell" id="cell' +alph[i]+ '"> </tr>');
+  console.log('<tr id="cell' +alph[i]+ '> </tr>');
+  }
+  for(var i = 0; i < x; i++){
+    curRow = "cell" +alph[i];
+    $("#"+curRow).append('<td class="cell" id="'+curRow + i +'"> </td>');
+  }
+}
+makeTable(8);
