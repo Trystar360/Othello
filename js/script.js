8e14cb1249830867e33f370fd07250a3bffb03d0
Trystar360 <tristanfrederick360@gmail.com>
2018-12-11 14:37:30 -0500

    update

--- js/script.js
+++ js/script.js
@@ -8,18 +8,33 @@ if(w < h){
 }else{
   gameDim = h;
 }
-$(".gameBoard").width(gameDim - 80);
-$(".gameBoard").height(gameDim - 80);
-console.log(gameDim);
+boardDim = gameDim - 80;
+$(".gameBoard").width(boardDim);
+$(".gameBoard").height(boardDim);
+
+//an array with the first 8 letters un the alphabet
 var alph = ["A","B","C","D","E","F","G","H"];
+
+//when called, creates a table that is x by x
 function makeTable(x){
+  //creates x rows
   for(var i = 0; i < x; i++){
-  $("#gameTable").append('<tr class="cell" id="cell' +alph[i]+ '"> </tr>');
+  $("#gameTable").append('<tr class="row" id="row' +alph[i]+ '"> </tr>');
   console.log('<tr id="cell' +alph[i]+ '> </tr>');
   }
-  for(var i = 0; i < x; i++){
-    curRow = "cell" +alph[i];
-    $("#"+curRow).append('<td class="cell" id="'+curRow + i +'"> </td>');
+  //creates x cells inside the rows
+  for(var j = 0; j < x; j++){
+    for(var i = 0; i < x; i++){
+      curRow = "row" +alph[j];
+      $("#"+curRow).append('<td class="cell" id="'+alph[j] + i +'"> </td>');
+    }
   }
+//sizes the cells
+var cellSize = boardDim/x;
+$("table").height(boardDim);
+$("table").width(boardDim);
+$(".cell").height(cellSize - 3.125);
+$(".cell").width(cellSize);
+
 }
 makeTable(8);
